5f145151f40efb4e79d9e0ee05e33698a6f78d70
Umai Balendra <youmaybal@gmail.com>
2021-01-16 20:28:43 -0500

    adjust react hooks and render basic data to app

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ const GOOGLE_API_KEY = 'AIzaSyDIYOa7B1fMDyEcBifamgtQvWmS4zoSTqw'
 function App() {
 
   const [query, setQuery] = useState('');
+  const [verifiedClaims, setVerifiedClaims] = useState([]); //mesha u can use this array of claims to render their urls on screen!
+  const [percentTrue, setPercentTrue] = useState(0);
+  const [percentFalse, setPercentFalse] = useState(0);
 
   const getFactData = () => {
     console.log(`Making API call to:\n\nhttps://factchecktools.googleapis.com/v1alpha1/claims:search?key=${GOOGLE_API_KEY}&query=trump did 9/11`)
@@ -15,34 +18,33 @@ function App() {
       then((data) => {
         console.log('data: ', data);
         console.log('claims: ', data.data.claims);
-        //store array of claims
-        var claimArray = data.data.claims;
-        var relevantClaims = []; //stores only False or True claims
         var trueCount = 0;
         var falseCount = 0;
         var totalSourceCount = 0;
+        var returnedClaims = (data.data.claims);
+        var relevantClaims = []; //stores only False or True claims
 
-        //grab only False or True-rated claims if there are any
-        if (claimArray != undefined && claimArray.length != 0) {
-          claimArray.forEach((arrayItem) => {
+        if (returnedClaims.length != 0) { //grab only False or True-rated claims if array is populated
+          returnedClaims.forEach((arrayItem) => {
             if (arrayItem.claimReview[0].textualRating == ("False") || arrayItem.claimReview[0].textualRating == ("True")) {
               relevantClaims.push(arrayItem.claimReview[0]);
+              setVerifiedClaims(verifiedClaims => [...verifiedClaims, arrayItem.claimReview[0]]);
             }
           });
 
-          //go through filtered claims and return textualRating Website
-          relevantClaims.forEach((claim) => {
+          relevantClaims.forEach((claim) => { //go through filtered claims and return textualRating Website
             if (claim.textualRating == "True") {
-              trueCount += 1;
+              trueCount+= 1;
             } else if (claim.textualRating == "False") {
-              falseCount += 1;
+              falseCount+= 1;
             }
             console.log("URL: " + claim.url + "  Rating: " + claim.textualRating); //print true/false and website
-            console.log("############");
           });
 
           totalSourceCount = trueCount + falseCount;
-          console.log("Based on " + totalSourceCount + "sources, this claim is " + trueCount + "true" + falseCount + "false");
+          setPercentTrue(trueCount/totalSourceCount * 100);
+          setPercentFalse(falseCount/totalSourceCount * 100);
+          console.log("Based on " + totalSourceCount + " sources, this claim is " + trueCount + " true " + falseCount + " false.");
         } else { //no claims in array
           console.log("We couldn't find any results, sorry about that");
         }
@@ -57,6 +59,9 @@ function App() {
       Fact Check - Hack the North 2021
       <input type="text" autoFocus="true" label="query" value={query} onChange={e => setQuery(e.target.value)} />
       <button onClick={getFactData} variant="primary">Get Data</button>
+      <text>True percent: {percentTrue}</text>
+      <text>False percent: {percentFalse}</text>
+      <text>length of verified claims array: {verifiedClaims.length}</text>
     </div>
   );
 }
