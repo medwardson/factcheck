720633feffa3154a8ad48579dd1518d70d978083
medwardson <mnaidooe@uwaterloo.ca>
2022-11-17 12:35:25 -0500

    Update port to 3000

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const axios = require('axios')
 const app = express()
-const port = 5000
+const port = 3000
 const cors = require('cors');
 
 const bearer = 'AAAAAAAAAAAAAAAAAAAAACxdLwEAAAAAMCd47gyu2r7pQ6EboheLNzED2o0%3DhToZZoq0JAJ3khR0reVFaxkmAk9EgCv9yZIFvANz0AUfzZ3LWR'
--- src/server/twitter.js
+++ src/server/twitter.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const getTweetByID = (tweetID) => {
   console.log('twitter api call ...');
   console.log('twitter id: ', tweetID);
-  return axios.get(`http://localhost:5000/factCheck?tweetID=${tweetID}`)
+  return axios.get(`http://localhost:3000/factCheck?tweetID=${tweetID}`)
   .then(response => {
       return response.data.data.data;
   }).catch(error => {
